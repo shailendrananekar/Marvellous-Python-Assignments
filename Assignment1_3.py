d1f7fab446aa500348d00b64ed13f69ec844a196
shailendrananekar <88242680+shailendrananekar@users.noreply.github.com>
2025-05-08 18:09:33 +0530

    Delete Assignment1_3.py
    folder organization.

--- Assignment1_3.py
+++
@@ -1,9 +0,0 @@
-def Add():
-    num1 = int(input("Enter First number: "))
-    num2 = int(input("Enter Second number: "))
-    result = num1 + num2
-    print("Addition of the two numbers is:", result)
-
-
-if __name__ == "__main__":
-    Add()
