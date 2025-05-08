aabc004a74d04f28923ff4d80934c1636b76f3b6
shailendrananekar <88242680+shailendrananekar@users.noreply.github.com>
2025-05-08 18:06:59 +0530

    Assignment_1

---
+++ Assignment_1/Assignment1_10.py
@@ -0,0 +1,10 @@
+def NameLength():
+
+    name = input("Enter your name: ")
+
+    length = len(name)
+    print("Length of your name is:", length)
+
+
+if __name__ == "__main__":
+    NameLength()
---
+++ Assignment_1/Assignment1_5.py
@@ -0,0 +1,7 @@
+def DisplayRange():
+
+    print(list(range(10, 0, -1)))
+
+
+if __name__ == "__main__":
+    DisplayRange()
