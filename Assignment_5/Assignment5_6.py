49c355edcb3a1149228ca48d956d7543418569b9
shailendrananekar <88242680+shailendrananekar@users.noreply.github.com>
2025-05-16 07:01:47 +0530

    Delete Assignment_5 directory

--- Assignment_5/Assignment5_6.py
+++
@@ -1,9 +0,0 @@
-def main():
-
-    num = int(input("Enter temprature in celsius : "))
-    Farehneit = (num * 9 / 5) + 32
-    print(f"Temprature in Farehneit is : {Farehneit}℉")
-
-
-if __name__ == "__main__":
-    main()
